8ce4706a8da4fa7a061105ac5feb3035cbeae7a5
theirix <theirix@gmail.com>
2018-03-10 22:50:22 +0300

    Find leptonica using more stable pkg-config

--- conanfile.py
+++ conanfile.py
@@ -37,13 +37,43 @@ class TesseractConan(ConanFile):
         cmake.definitions['BUILD_TRAINING_TOOLS'] = False
         cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
         cmake.definitions["STATIC"] = not self.options.shared
-        # it's required to set this variable.
-        # otherwise tesseract uses pkg-config which cannot find static leptonica
-        cmake.definitions['Leptonica_DIR'] = self.deps_cpp_info['leptonica'].rootpath
 
-        cmake.configure(source_folder=self.source_subfolder)
-        cmake.build()
-        cmake.install()
+        # provide patched lept.pc
+        shutil.copy(os.path.join(self.deps_cpp_info['leptonica'].rootpath, 'lib', 'pkgconfig', 'lept.pc'), 'lept.pc')
+        tools.replace_prefix_in_pc_file("lept.pc", self.deps_cpp_info['leptonica'].rootpath)
+
+        # if static leptonica used, tesseract must use Leptonica_STATIC_LIBRARIES
+        # which use static dependencies like jpeg, png etc provided by lept.pc
+        if not self.options['leptonica'].shared:
+            tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeListsOriginal.txt"),
+                    "target_link_libraries       (libtesseract ${Leptonica_LIBRARIES})",
+                    "target_link_libraries       (libtesseract ${Leptonica_STATIC_LIBRARIES})")
+
+        if self.version == "3.05.01":
+            # upstream bug: output name is not substituted for tesseract.pc
+            # fixed in master but still an issue for stable
+            tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeListsOriginal.txt"),
+                    "set_target_properties           (libtesseract PROPERTIES DEBUG_OUTPUT_NAME tesseract${VERSION_MAJOR}${VERSION_MINOR}d)",
+                    "set_target_properties           (libtesseract PROPERTIES DEBUG_OUTPUT_NAME tesseract${VERSION_MAJOR}${VERSION_MINOR}d)\n"
+                    "else()\n"
+                    "set_target_properties           (libtesseract PROPERTIES OUTPUT_NAME tesseract)\n")
+
+        with tools.environment_append({'PKG_CONFIG_PATH': self.build_folder}):
+            cmake.configure(source_folder=self.source_subfolder)
+            cmake.build()
+            cmake.install()
+
+        # Fix pc file: cmake does not fill libs.private
+        if self.settings.os != 'Windows':
+            libs_private = []
+            for dep in ['leptonica']:
+                libs_private.extend(['-L'+path for path in self.deps_cpp_info[dep].lib_paths])
+                libs_private.extend(['-l'+lib for lib in self.deps_cpp_info[dep].libs])
+            path = os.path.join(self.package_folder, 'lib', 'pkgconfig', 'tesseract.pc')
+            tools.replace_in_file(path,
+                                 'Libs.private:',
+                                 'Libs.private: ' + ' '.join(libs_private))
+
 
     def build(self):
         if self.settings.compiler == "Visual Studio":
