a47922f731fad2dbbe3ecc5ecfb37ef4433f4e08
theirix <theirix@gmail.com>
2018-10-21 01:14:29 +0300

    Updated ci conventions

--- .ci/install.sh
+++ .ci/install.sh
@@ -8,14 +8,13 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew outdated pyenv || brew upgrade pyenv
     brew install pyenv-virtualenv
     brew install cmake || true
-    brew install pkg-config || true
 
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
+    pyenv install 3.7.0
+    pyenv virtualenv 3.7.0 conan
     pyenv rehash
     pyenv activate conan
 fi
